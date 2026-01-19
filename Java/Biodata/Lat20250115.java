ed5132f6cdc6d33733f9e291f20e560f9365f57e
Jason Christopher Hukom <me@jasonhukom.my.id>
2026-01-19 08:29:45 +0700

    Update 12Jan2026

---
+++ Java/Biodata/Lat20250115.java
@@ -0,0 +1,16 @@
+public class Lat20250115 {
+	public static void main(String [] x) {
+		System.out.println("=====PERSONAL=====");
+		System.out.println("*) Name: Jason Christopher Hukom");
+		System.out.println("*) Email: me@jasonhukom.my.id");
+		System.out.println("*) Date of Birth: 16 May 2010");
+		System.out.println("*) Gender: Male");
+		System.out.println("*) Nationality: Indonesian");
+		System.out.println("*) Religion: Christian");
+		System.out.println("*) Languages known: English, Indonesian");
+		System.out.println("");
+		System.out.println("=====EDUCATIONAL BACKGROUND");
+		System.out.println("*) High School: Vocational High School 2 Surakarta");
+		System.out.println("*) Major: Software Engineering");
+	}
+}
---
+++ Java/Loop/Lat20250114.java
@@ -0,0 +1,9 @@
+public class loop1 {
+	public static void main(String [] x) {
+		int loop = 0;
+		while (loop < 15) {
+			System.out.println("Well");
+			loop++;
+		}
+	}
+}
