3ab8d3c174b488f30c9baebd73a557239864068b
Jason Christopher Hukom <me@jasonhukom.my.id>
2026-01-28 14:51:38 +0700

    Update Math Jan 28 2026

---
+++ Java/Math/Geometry/src/Fasculator.java
@@ -0,0 +1,8 @@
+public class Fasculator {
+	public static void main(String[] args) {
+		final double piValue = Math.PI; // const value for pi
+		System.out.println("===PRISCULATOR===");
+		System.out.println("counts your prisms");
+		System.out.println();
+	}
+}
