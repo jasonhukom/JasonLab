4763010a243e0ee7a5eb2e9d677c25a9e752ded3
Jason Christopher Hukom <me@jasonhukom.my.id>
2026-01-29 16:42:03 +0700

    Update Fasculator 29 Jan 2026

--- Java/Math/Geometry/src/Fasculator.java
+++ Java/Math/Geometry/src/Fasculator.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Fasculator {
 	public static void main(String[] args) {
-		final double piValue = Math.PI; // const value for pi
+		Scanner input = new Scanner(System.in);
+
+		float side1, side2, height;
+
 		System.out.println("===PRISCULATOR===");
 		System.out.println("counts your prisms");
 		System.out.println();
+		System.out.println("Side 1: ");
+
+		// AREA BASE DOUBLE/INT
+		double areaBaseDouble = 1/2 * (side1 * side2) *  (height ** 2);
+		int areaBaseInt = areaBaseDouble
+
+		// VOLUME DOUBLE/INT
+		double volumeDouble = 
 	}
 }
+
