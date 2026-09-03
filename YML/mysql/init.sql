f23737ef7b168564bcc6a970e216694a3498c957
jason hukom <jasonhukom9@gmail.com>
2026-09-03 08:47:46 +0700

    Added Tragedi 9/11

--- YML/mysql/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER '<mysql_username>'@'%' IDENTIFIED BY '<mysql_password>';
-CREATE USER '<mysql_username>'@'phpmyadmin' IDENTIFIED BY '<mysql_password>';
-GRANT ALL PRIVILEGES ON *.* TO '<mysql_username>'@'%' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO '<mysql_username>'@'phpmyadmin' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
